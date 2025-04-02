772f0d9ecf131e3eb14c9c1f7d5a671211e7053c
ferer00 <ferer004829@gmail.com>
2025-04-02 19:19:29 +0300

    Add main() and listToString() methods to DoublyLinkedList

--- src/main/java/DoublyLinkedList.java
+++ src/main/java/DoublyLinkedList.java
@@ -216,4 +216,59 @@ public class DoublyLinkedList {
         size += copy.size;
     }
 
+
+        public static void main(String[] args) {
+            DoublyLinkedList list = new DoublyLinkedList();
+            
+            list.append('A');
+            list.append('B');
+            list.append('C');
+            System.out.println("Після додавання A, B, C: " + listToString(list));
+            
+            list.insert('D', 1);
+            System.out.println("Після вставки D на позицію 1: " + listToString(list));
+            
+            char deleted = list.delete(2);
+            System.out.println("Видалено елемент на позиції 2: " + deleted);
+            System.out.println("Після видалення: " + listToString(list));
+            
+            System.out.println("Перше входження 'A': " + list.findFirst('A'));
+            System.out.println("Останнє входження 'B': " + list.findLast('B'));
+            
+            list.append('A');
+            list.append('B');
+            list.append('A');
+            System.out.println("Після додавання A, B, A: " + listToString(list));
+            
+            list.deleteAll('A');
+            System.out.println("Після видалення всіх 'A': " + listToString(list));
+            
+            DoublyLinkedList copy = list.clone();
+            System.out.println("Копія списку: " + listToString(copy));
+            
+            list.reverse();
+            System.out.println("Після обернення: " + listToString(list));
+            
+            list.clear();
+            System.out.println("Після очищення, довжина: " + list.length());
+            
+            list.extend(copy);
+            System.out.println("Після розширення копією: " + listToString(list));
+        }
+        
+        
+        private static String listToString(DoublyLinkedList list) {
+            StringBuilder sb = new StringBuilder("[");
+            try {
+                for (int i = 0; i < list.length(); i++) {
+                    if (i > 0) sb.append(", ");
+                    sb.append(list.get(i));
+                }
+            } catch (IndexOutOfBoundsException e) {
+                return "Error: " + e.getMessage();
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+
 }
